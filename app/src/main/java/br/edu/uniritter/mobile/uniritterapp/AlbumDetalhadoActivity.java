e76fd124d3832d15986e0dbe1495860b5a6a3d3b
MatheusLMallmann <matheusl.mallmann@gmail.com>
2021-04-04 12:51:52 -0300

    Atividade 2 sem JSON dos USERS

---
+++ app/src/main/java/br/edu/uniritter/mobile/uniritterapp/AlbumDetalhadoActivity.java
@@ -0,0 +1,14 @@
+package br.edu.uniritter.mobile.uniritterapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlbumDetalhadoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_album_detalhado);
+    }
+}
